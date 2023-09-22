543a56bbeb8e515e216a87c21a09f41de8c69e64
Nivita <nnp25@case.edu>
2023-09-22 17:50:33 -0400

    created EventMain class

---
+++ EventMain.java
@@ -0,0 +1,27 @@
+/*
+ * Class interacts directly with the command-line,
+ * holds functions that call validator and SQLite functions.
+ */
+public class EventMain{
+
+    /* Validates entry and creates an event in Events table */
+    public static void addEvent(){
+        //validate entry
+
+        //SQL command to insert
+    }
+   
+    /* Validates entry and creates a participant in Participants table */
+    public static void registerParticipant(){
+        //validate entry
+
+        //SQL command to insert
+    }
+
+    /* Returns a table of participants currently registered */
+    public static void viewParticipants(){
+        //SQL command to view table
+        
+    }
+
+}
